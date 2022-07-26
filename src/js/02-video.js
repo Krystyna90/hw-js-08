201414fb6f04a1679d6aebed7c346762739513a8
ukrbride4u <ukrbride4u@gmail.com>
2022-07-26 16:43:22 +0300

    try-one

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iFrameEl = document.querySelector('#vimeo-player');
+const videoPlayer = new Player(iFrameEl);
+
+videoPlayer.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+videoPlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
